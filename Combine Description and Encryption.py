10c0b6f778a226cbcc4008a1f246d7757e22299d
Rishthegreat <rishitarora2005@gmail.com>
2021-01-23 13:40:45 -0500

    Merge remote-tracking branch 'origin/master'

--- Combine Description and Encryption.py
+++ Combine Description and Encryption.py
@@ -324,12 +324,12 @@ def decode():
             decode_onedig.pop()
             decode_twodig.pop()
             decode_bases.pop()
-            print(decode_twodig)
         elif decode_pathway[y] == "b":
             decode_code = tobase(str(decode_code), int(decode_bases[len(decode_bases) - 1]),
                                  int(decode_bases[len(decode_bases) - 2]))
             decode_bases.pop()
         elif decode_pathway[y] == "d":
+
             print("runthroughcipher")
     print(decode_code)
 
